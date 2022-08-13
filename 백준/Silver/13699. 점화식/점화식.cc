8971506e780372104b00c96a9de6d9fd13dfc254
nmy6452 <85818940+nmy6452@users.noreply.github.com>
2022-08-13 15:37:57 +0900

    [Silver IV] Title: 점화식, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/13699. 점화식/점화식.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+unsigned long long DP[36] = {1,};
+
+int main() {
+	int N;
+	cin >> N;
+
+	for (int i = 1; i <= N; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			DP[i] = DP[i] + DP[j] * DP[i - j - 1];
+		}
+	}
+
+	cout << DP[N];
+
+}
