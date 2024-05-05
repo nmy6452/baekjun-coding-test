4a0ee66a1ef5c6e67ebb9cfdbfea5591d01f0b68
노민영 <nmy6452@naver.com>
2024-05-05 17:49:58 +0900

    주석 추가
    주석 추가

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -5,6 +5,16 @@ import java.util.*;
 
 brute force 하게 풀이시 시간초과
 정렬해서 비교해보자
+
+자바 정렬 알고리즘 시간복잡도 비교
+
+Arrays.sort()
+DualPivotQuicksort
+평균 : O(nlog(n)) / 최악 : O(n^2)
+
+Collections.sort()
+TimeSort (삽입정렬과 합병정렬을 결합한 정렬)
+평균, 최악 : O(nlog(n))
 */
 
 class Solution {
