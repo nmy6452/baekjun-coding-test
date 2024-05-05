acb02c18505b3dd5b611b094b28e68734c239a72
노민영 <nmy6452@naver.com>
2024-05-05 15:40:52 +0900

    주석 추가
    주석 추가

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -8,15 +8,15 @@ class Solution {
         
         //가져갈 수 있는 포켓몬의 갯수
        for(int i =0 ;i<nums.length; i++){
-           if(list.size() >= max){
+           if(list.size() >= max){        //만약 가져갈 수 있는 포겟몬의 수를 초과할 경우 for Loop 탈출
                break;
            }
-           if(list.contains(nums[i])){
+           if(list.contains(nums[i])){     //이미 가지고 있는 포켓몬을 만날 경우 해당 무시 
                continue;
-           }else{
+           }else{                          //가지고 있지 않는 포켓몬을 만날 경우 포켓몬 획득
                list.add(nums[i]);
            }
        }
-        return list.size();
+        return list.size();                //가지고 갈 수 있는 최대의 포켓몬 종류를 출력
     }
 }
