f114aaf3f7efc0d7dc7f4f50aff7683746cff9ff
노민영 <nmy6452@naver.com>
2025-08-10 01:44:29 +0900

    Update 입국심사.java

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,23 +1,26 @@
 import java.util.*;
 class Solution {
-        public long solution(int n, int[] times) {
+    public long solution(int n, int[] times) {
         long answer = 0;
         Arrays.sort(times);
-        long left = 0;
-        long right = times[times.length-1] * (long)n; //모든 사람이 가장 느리게 심사받음
         
-        while(left <= right) {
-            long mid = (left + right) / 2;
-            long complete = 0;
-            for (int i = 0; i < times.length; i++)
-                complete += mid / times[i];
-            if (complete < n) // 해당 시간에는 모든 사람이 검사받을 수 없다.
-                left = mid + 1;
-            else {
-                right = mid - 1;
-                answer = mid; // 모두 검사받았으나, 더 최솟값이 있을 수 있다.
+        
+        long Min = 0;
+        long Max = times[times.length -1] * (long)n;
+        
+        while(Min <= Max){
+            long Mid = (Max+Min) / 2;
+            long cnt = 0;
+            for(int i = 0; i<times.length; i++){
+                cnt += Mid / times[i];
             }
-        }  
+            if(cnt < n){
+                Min = Mid + 1;
+            }else{
+                Max = Mid - 1;
+                answer = Mid;
+            }
+        }
         return answer;
     }
 }
